fa0af17bf11bb1070fdd9be4d690ca3fe6c3cb35
VanHakobyan <van19962013@mail.ru>
2017-01-29 18:10:13 +0400

    add customer

--- AutoCustomer/AutoCustomer/Customer.cs
+++ AutoCustomer/AutoCustomer/Customer.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace AutoCustomer
 {
-    class Customer
+    struct Customer
     {
+        readonly string nameCustomer;
+        readonly string model;
+        readonly string phoneNumber;
+
+        public Customer(string nameCustomer, string model, string phoneNumber)
+        {
+            this.nameCustomer = nameCustomer;
+            this.model = model;
+            this.phoneNumber = phoneNumber;
+
+        }
+
+
+        public string NameCustomer { get { return nameCustomer; } }
+        public string Model { get { return model; } }
+        public string PhoneNumber { get { return phoneNumber; } }
+
+
+        public override string ToString()
+        {
+            return $"Name Customer: {nameCustomer} Model:{model} phone number: {phoneNumber}";
+        }
+
     }
 }
